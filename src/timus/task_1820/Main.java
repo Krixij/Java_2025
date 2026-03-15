8eaa165232567453ce85273db8822195fc034dd8
krixij <Kutorov.ivan@mail.ru>
2026-03-15 23:23:50 +0500

    Lr9

---
+++ src/timus/task_1820/Main.java
@@ -0,0 +1,13 @@
+package timus.task_1820;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int A = in.nextInt();
+        int B = in.nextInt();
+        System.out.println(N*(A*B*2));
+    }
+}
